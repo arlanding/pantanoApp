b8387f227e31feb33a0dfce5e96f2fd0e0a2c0fb
arlanding <fedefabre@MacBook-Pro-de-Federico.local>
2019-03-29 01:15:39 -0300

    observers added

--- src/app/game/components/question/question.component.ts
+++ src/app/game/components/question/question.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'pantano-question',
@@ -6,9 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
+  @Input() game;
+
+  private $questionSubscription;
+  public question: string = '';
+  public answer1 = '';
+  public answer2 = '';
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+   debugger;
+   this.$questionSubscription = this.game.subscribe( newQuestion => {
+    debugger;
+    this.renderQuestion(newQuestion);
+   });
+  }
+
+  renderQuestion(question){
+    this.question = question.quest;
+    this.answer1 = question.ans;
+    this.answer2 = 'fail '+ question.ans;
+  }
 
 }
--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -1,18 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from './services/game.service';
+import { Subject } from 'rxjs';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.page.html',
   styleUrls: ['./game.page.scss'],
+  providers: [ GameService, AlertController ]
 })
 export class GamePage implements OnInit {
 
-  win = false;
-  fail = false;
+  private newGameSubj: Subject<boolean> = new Subject();
+  public game$ = this.newGameSubj.asObservable();
 
-  constructor() { }
+  constructor(private gameService: GameService, public alertController: AlertController) { }
 
   ngOnInit() {
+    this.gameService.getGame().subscribe( gameQuestions => {
+      // this.presentAlert();
+      this.startNewGame(gameQuestions);
+    });
+  }
+  
+/*   async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  } */
+  
+
+  private startNewGame(questions){
+    console.log(questions);
+    setTimeout(()=>{
+      this.nofityNewGame(questions[0]);
+    }, 3000);
+  }
+
+  private nofityNewGame(newQuestion){
+    debugger;
+    this.newGameSubj.next(newQuestion);    
   }
 
 }
